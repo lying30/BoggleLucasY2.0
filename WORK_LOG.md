cba45919daba06ba2be4c72f15fc740a350c4a73
lucas.ying@menloschool.org <lucas.ying@menloschool.org>
2025-01-08 23:56:59 -0800

    update

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,10 @@
-Name: YOUR NAME HERE
+Name: Lucas Ying
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
+| Date  |    Time     |                                                                                                                                                     Update |
+|:------|:-----------:|-----------------------------------------------------------------------------------------------------------------------------------------------------------:|
+| Jan 7 |    2-3pm    |       I reloaded my repo with the corrected tester files and then started to code a little bit based on what we talked about in class about recursive DFS. |
+| Jan 8 | 10:30-11:55 | Established the methods I think i might use and implemented a trie for the words and began to implement the DFS method explore like we went over in class. |
+|       |             |                                                                                                                                                            |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/Boggle.java
+++ src/Boggle.java
@@ -3,17 +3,44 @@ import java.util.Arrays;
 
 public class Boggle {
 
+    public static class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        boolean isEndOfWord = false;
+
+        public void insert (String word){
+
+        }
+        public boolean startsWith(String prefix) {
+            return true;
+        }
+        public boolean search(String word){
+            return true;
+        }
+    }
+
     public static String[] findWords(char[][] board, String[] dictionary) {
 
-        ArrayList<String> goodWords = new ArrayList<String>();
+       TrieNode trie = new TrieNode();
+       for (String word: dictionary) {
+           trie.insert(word);
+       }
+       
+       ArrayList<String> goodWords = new ArrayList<String>();
+       boolean[][] visited = new boolean[board.length][board[0].length];
 
-        // TODO: Complete the function findWords(). Add all words that are found both on the board
-        //  and in the dictionary.
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                explore(board, i, j, visited, trie, "", goodWords);
+            }
+        }
 
         // Convert the list into a sorted array of strings, then return the array.
         String[] sol = new String[goodWords.size()];
         goodWords.toArray(sol);
         Arrays.sort(sol);
         return sol;
+    }
+    public static void explore(char[][] board, int x, int y, boolean[][] visited, TrieNode trie, String word, ArrayList<String> goodWords){
+        if ()
     }
 }
